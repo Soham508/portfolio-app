0ddf5f149658c7fcebabbb56e95af45a2288fe58
Soham <sohamghige508@gmail.com>
2025-12-15 05:02:11 +0530

    logo update

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Portfolio App",
   description: "Portfolio App",
+  icons: {
+    icon: "/assets/logo.png",
+  },
 };
 
 export default function RootLayout({
